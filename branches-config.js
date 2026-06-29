014c0c28ae1458fad596a7a4c8d26a3a3d395716
habibiarmin27-star <habibiarmin27@gmail.com>
2026-06-29 19:15:37 +0400

    Add files via upload

---
+++ branches-config.js
@@ -0,0 +1,7 @@
+// branches-config.js
+// List of shop branches. Add more branches here later — nothing else needs to change.
+
+export const BRANCHES = [
+  "Al Hudu Abu Dhabi",
+  "Al Hudu Dubai",
+];
